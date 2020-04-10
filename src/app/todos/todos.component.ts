76c284cd8a83b46d9d74fb6049fb5408a835b00c
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-04-10 11:16:04 +0300

    Adding fade in  effect

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
+import {trigger} from '@angular/animations';
+import {transition} from '@angular/animations';
+import {state} from '@angular/animations';
+import {style} from '@angular/animations';
+import {animate} from '@angular/animations';
 
 @Component({
   selector: 'todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
+  animations: [
+    trigger('fade',[
+      transition('void => *', [
+        style({opacity: 0}),
+        animate(2000)
+      ])
+    ])
+  ]
 })
 export class TodosComponent {
   items: any[] = [
-    'Wash the dishes', 
-    'Call the accountant', 
+    'Wash the dishes',
+    'Call the accountant',
     'Apply for a car insurance'];
 
   addItem(input: HTMLInputElement) {
     this.items.splice(0, 0, input.value);
-    input.value = ''; 
+    input.value = '';
   }
 
   removeItem(item) {
