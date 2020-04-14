15c4ed8eb744e2d96516a41bf409373511444259
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-04-14 16:30:50 +0300

    Adding sippy animation

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -1,15 +1,35 @@
 import { Component, Input } from '@angular/core';
+import {animationExp} from '../animation/animation';
+import {useAnimation} from '@angular/animations';
+import {animation} from '@angular/animations';
+import {trigger} from '@angular/animations';
+import {transition} from '@angular/animations';
+import {state} from '@angular/animations';
+import {style} from '@angular/animations';
+import {animate} from '@angular/animations';
+import {keyframes} from '@angular/animations';
+import {query} from '@angular/animations';
+import {animateChild} from '@angular/animations';
+import {group} from '@angular/animations';
+import {stagger} from '@angular/animations';
 
 @Component({
   selector: 'zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.css'],
+  animations: [
+    trigger('fade',[
+      state('expanded', style({height: '*',overflow: 'auto'})),
+      state('notexpanded',style({height: 0,overflow:'hidden',paddingTop: 0, paddingBottom: 0})),
+      transition('notexpanded => expanded',[animate(500)]),
+      transition('expanded => notexpanded',[animate(500)])
+    ])
+  ]
 })
 export class ZippyComponent  {
   @Input('title') title: string;
   isExpanded: boolean;
-
-  toggle() { 
+  toggle() {
     this.isExpanded = !this.isExpanded;
   }
 }
